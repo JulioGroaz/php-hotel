5f20e555945297442a7b9e692ccc29dd67db069d
JulioGroaz <julio.groaz2020@gmail.com>
2024-06-27 20:18:08 +0200

    milestone 2 fatta

--- index.php
+++ index.php
@@ -7,33 +7,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
+    <header>
+    <form method="GET" action="">
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="parking" name="parking" <?php if (isset($_GET['parking'])) echo 'checked'; ?>>
+                <label class="form-check-label" for="parking">Mostra solo hotel con parcheggio</label>
+            </div>
+            <button type="submit" class="btn btn-primary mt-2">Filtra</button>
+        </form>
+    </header>
     <main>
+    
     <?php
     include __DIR__ .'/arrayHotels.php';
 
-   echo "<table class='table'>";
-    echo "<thead>";
-      echo "<tr>";
-      echo "<th scope='col'>Nome</th>";
-      echo "<th scope='col'>Descrizione</th>";
-      echo "<th scope='col'>Parcheggio</th>";
-      echo "<th scope='col'>Voto</th>";
-      echo "<th scope='col'>Distanza dal centro (KM)</th>";
-    echo "</tr>";
-  echo "</thead>";
-  echo "<tbody class='table-group-divider'>";
-  
-  foreach ($hotels as $hotel){
-    echo"<tr>";
-      echo"<th scope='row'>".($hotel['name'])."</th>";
-      echo"<td>".$hotel['description']."</td>";
-      echo"<td>".$hotel['parking']."</td>";
-      echo"<td>".$hotel['vote']."</td>";
-      echo"<td>".$hotel['distance_to_center']."</td>";
-    echo"</tr>";
-}
-  echo"</tbody>";
-echo"</table>";
+    if (isset($_GET['parking'])){
+        $hotels = array_filter($hotels,function($hotel){
+            return $hotel['parking'];
+        });
+    }
+
+    echo "<table class='table'>";
+        echo "<thead>";
+            echo "<tr>";
+                echo "<th scope='col'>Nome</th>";
+                echo "<th scope='col'>Descrizione</th>";
+                echo "<th scope='col'>Parcheggio</th>";
+                echo "<th scope='col'>Voto</th>";
+                echo "<th scope='col'>Distanza dal centro (KM)</th>";
+            echo "</tr>";
+        echo "</thead>";    
+        echo "<tbody class='table-group-divider'>";
+    
+            foreach ($hotels as $hotel){
+                    echo"<tr>";
+                        echo"<th scope='row'>".($hotel['name'])."</th>";
+                        echo"<td>".$hotel['description']."</td>";
+                        echo"<td>".$hotel['parking']."</td>";
+                        echo"<td>".$hotel['vote']."</td>";
+                        echo"<td>".$hotel['distance_to_center']."</td>";
+                    echo"</tr>";
+                }
+        echo"</tbody>";
+    echo"</table>";
 ?>
     </main>
 
